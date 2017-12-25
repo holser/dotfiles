7825c699b105d194b9ae711df7addfb767f3d746
Sergii Golovatiuk <sgolovat@redhat.com>
2017-12-25 15:34:48 +0100

    Continue refactoring
    * Remove .bash* .inputrc as I use zsh only
    * Move .brew to install_brew.sh
    * Modify gitconfig
    * Fix tmuxrc in accordance with latest tmux changes
    * Remove .wgetrc
    * Move .osx to configure_osx.sh
    * Add homebrew in .zpreztorc
    * Move keys back to emacs in .zpreztorc

---
+++ install_brew.sh
@@ -0,0 +1,75 @@
+#!/usr/bin/env bash
+
+# Install Homebrew
+ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+# Ask for the administrator password upfront
+sudo -v
+
+# Keep-alive: update existing `sudo` time stamp until the script has finished
+while true; do sudo -n true; sleep 60; kill -0 "$$" || exit; done 2>/dev/null &
+
+# Make sure we’re using the latest Homebrew.
+brew update
+# Upgrade any already-installed formulae.
+brew upgrade
+
+# Install command-line tools using Homebrew.
+brew install ansible
+brew install aria2
+brew install arping
+brew install aspell
+brew install binwalk
+brew install coreutils
+brew install ctags
+brew install curl
+brew install findutils # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install gawk
+brew install git
+brew install git-review
+brew install gnu-sed
+brew install grep
+brew install gpg
+brew install htop
+brew install jq
+brew install mas
+brew install moreutils # Install GNU `find`, `locate`, `updatedb`, and `xargs`, `g`-prefixed.
+brew install mtr
+brew install neovim
+brew install nmap
+brew install openssl
+brew install p7zip
+brew install pv
+brew install python
+brew install python3
+brew install reattach-to-user-namespace
+brew install rsync
+brew install ssh-copy-id
+brew install tmux
+brew install vagrant-completion
+brew install watch
+brew install wget --enable-iri
+brew install xz
+brew cleanup
+
+brew cask install aerial
+brew cask install blue-jeans
+brew cask install chromium
+brew cask install google-hangouts
+brew cask install hazel
+brew cask install iterm2
+brew cask install jollysfastvnc
+brew cask install little-snitch
+brew cask install mpv
+brew cask install vagrant
+brew cask install virtualbox
+brew cask install viscosity
+brew cask install xld
+brew cask install xquartz
+brew cask cleanup
+
+mas install 747648890  # Telegram
+mas install 443987910  # 1Password
+mas install 1262957439 # Textual 7
+mas install 803453959  # Slack
+mas install 407963104  # Pixelmator
